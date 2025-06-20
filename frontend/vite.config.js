053771a9e2f61bb0808b50657fc92bc0acca4695
Debjit Dey <debjitdeyofficial6@gmail.com>
2025-06-20 18:58:27 +0530

    vercel deploy configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // default is fine unless deploying to subpath
   plugins: [react(), tailwindcss()],
 });
